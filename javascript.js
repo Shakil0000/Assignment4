28fe135d0681ec6791f49a43f1890284e1083aa4
Shakil0000 <shakiazzaman.s@gmail.com>
2021-01-30 18:23:20 +0600

    2nd commit

--- javascript.js
+++ javascript.js
@@ -1,134 +1,64 @@
-document.getElementById("minusBtnFirst").addEventListener("click",function(){
-    var currentInputFirst = document.getElementById("currentInputFirst").value;
-    var inputFloat = parseFloat(currentInputFirst);
-    if (inputFloat <= 0) {
-        inputFloat = 0;
+                               // For Minus Button
+function minusButton(id){
+    var currentInpt = document.getElementById(id).value;
+    var currentInptFloat = parseFloat(currentInpt);
+
+    if (currentInptFloat <= 0) {
+          currentInptFloat = 0;
     } 
     else {
-        inputFloat--;   
+        currentInptFloat--;
     }
-   document.getElementById("currentInputFirst").value=inputFloat;
+    document.getElementById(id).value = currentInptFloat;
 
-   var firstInput = document.getElementById("currentInputFirst").value;
-   var firstInputFloat = parseFloat(firstInput) * 150;
-   var secondInput = document.getElementById("currentInputSecond").value;
-   var secondInputFloat = parseFloat(secondInput) * 100;
-   var subTotal = firstInputFloat + secondInputFloat;
-   document.getElementById("subTotal").innerText = subTotal;
-   var vat = subTotal/10.0;
-   document.getElementById("vat").innerText = vat;
-   var total = subTotal + vat;
-   document.getElementById("total").innerText = total;
-});
+    subtotalVatTotal();
+
+ }
 
 
+                             //For Plus Button
+function plusButton(id){
+    var currentInpt = document.getElementById(id).value;
+    var currentInptFloat = parseFloat(currentInpt);
 
+    
+        currentInptFloat++;
+    
+    document.getElementById(id).value = currentInptFloat;
 
+    subtotalVatTotal();
+
+}
+
+                            // For subtotal,Vat,Total 
+function subtotalVatTotal(){
+            var firstInput = document.getElementById("currentInputFirst").value;
+            var firstInputFloat = parseFloat(firstInput) * 150;
+            var secondInput = document.getElementById("currentInputSecond").value;
+            var secondInputFloat = parseFloat(secondInput) * 100;
+            var subTotal = firstInputFloat + secondInputFloat;
+            document.getElementById("subTotal").innerText = subTotal;
+            var vat = subTotal/10.0;
+            document.getElementById("vat").innerText = vat;
+            var total = subTotal + vat;
+            document.getElementById("total").innerText = total;
+ }
 
 
 document.getElementById("plusBtnFirst").addEventListener("click",function(){
-    var currentInputFirst = document.getElementById("currentInputFirst").value;
-    var inputFloat = parseFloat(currentInputFirst);
-        inputFloat++;   
-   document.getElementById("currentInputFirst").value=inputFloat;
-
-   var firstInput = document.getElementById("currentInputFirst").value;
-   var firstInputFloat = parseFloat(firstInput) * 150;
-   var secondInput = document.getElementById("currentInputSecond").value;
-   var secondInputFloat = parseFloat(secondInput) * 100;
-   var subTotal = firstInputFloat + secondInputFloat;
-   document.getElementById("subTotal").innerText = subTotal;
-   var vat = subTotal/10.0;
-   document.getElementById("vat").innerText = vat;
-   var total = subTotal + vat;
-   document.getElementById("total").innerText = total;
+    plusButton("currentInputFirst");
 });
-
-
-
-
-
-
-//second section
-
-
-
-
-
-
-
-document.getElementById("minusBtnSecond").addEventListener("click",function(){
-    var currentInputFirst = document.getElementById("currentInputSecond").value;
-    var inputFloat = parseFloat(currentInputFirst);
-    if (inputFloat <= 0) {
-        inputFloat = 0;
-    } 
-    else {
-        inputFloat--;   
-    }
-   document.getElementById("currentInputSecond").value=inputFloat;
-
-   
-   var firstInput = document.getElementById("currentInputFirst").value;
-   var firstInputFloat = parseFloat(firstInput) * 150;
-   var secondInput = document.getElementById("currentInputSecond").value;
-   var secondInputFloat = parseFloat(secondInput) * 100;
-   var subTotal = firstInputFloat + secondInputFloat;
-   document.getElementById("subTotal").innerText = subTotal;
-   var vat = subTotal/10.0;
-   document.getElementById("vat").innerText = vat;
-   var total = subTotal + vat;
-   document.getElementById("total").innerText = total;
+document.getElementById("minusBtnFirst").addEventListener("click",function(){
+    minusButton("currentInputFirst");
 });
-
-
-
-
-
-
 document.getElementById("plusBtnSecond").addEventListener("click",function(){
-    var currentInputFirst = document.getElementById("currentInputSecond").value;
-    var inputFloat = parseFloat(currentInputFirst);
-        inputFloat++;   
-   document.getElementById("currentInputSecond").value=inputFloat;
-
-   var firstInput = document.getElementById("currentInputFirst").value;
-   var firstInputFloat = parseFloat(firstInput) * 150;
-   var secondInput = document.getElementById("currentInputSecond").value;
-   var secondInputFloat = parseFloat(secondInput) * 100;
-   var subTotal = firstInputFloat + secondInputFloat;
-   document.getElementById("subTotal").innerText = subTotal;
-   var vat = subTotal/10.0;
-   document.getElementById("vat").innerText = vat;
-   var total = subTotal + vat;
-   document.getElementById("total").innerText = total;
+    plusButton("currentInputSecond");
 });
-
+document.getElementById("minusBtnSecond").addEventListener("click",function(){
+    minusButton("currentInputSecond");
+});
 
 
 document.getElementById("bookNowBtn").addEventListener("click",function(){
     alert("Booking Success.Thank You.");
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
